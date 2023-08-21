3c83e343e069a0aeb25038f77db1ce9a54e551ae
moayadsamy <7404f86580@gmail.com>
2023-08-21 16:40:12 +0300

    shopping cart js

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+let shopItemsData = [
+   {
+    id:  "jfhgbvnscs",
+    name: "Laptop",
+    price: 1000,
+    desc: "لابتوب بمواصفات عالية",
+    category: "computers",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrlj8luiqhZA-PyP4Chvs2mMp3LabcOLWjSp27RKti&s",
+    count: 20,
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Laptop Hp",
+    price: 900,
+    desc: "افضل اجهزة لابتوب ماركة HP",
+    category: "computers",
+    img:
+      "https://www.3allemni.com/wp-content/uploads/2020/04/%D8%A7%D9%81%D8%B6%D9%84-%D8%A7%D8%AC%D9%87%D8%B2%D8%A9-%D9%84%D8%A7%D8%A8%D8%AA%D9%88%D8%A8-%D9%85%D8%A7%D8%B1%D9%83%D8%A9-HP-%D9%84%D8%B9%D8%A7%D9%85-2020-2-450x360.png",
+    count: 20,
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Laptop Lenovo ",
+    price: 1200,
+    desc: "أفضل أجهزة اللاب توب من Lenovo",
+    category: "computers",
+    img:
+      "https://m.media-amazon.com/images/I/413WfgQnLVL._AC_UF1000,1000_QL80_.jpg",
+    count: 20,
+  },
+  {
+    id: "hyfhcbcv",
+    name: "Laptop DELL",
+    price: 700,
+    desc: "أفضل أجهزة اللاب توب من Dell",
+    category: "computers",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST4ZoYyNNDhiScJlQwcM_3h7m1aqYLt6srf6UoP8-NGw1ycq3nEWZQ9d7C_HNcgGNLR8w&usqp=CAU",
+    count: 20,
+  },
+];
